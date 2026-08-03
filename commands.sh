8cf3746dc18c6a92d3b2ff7e433510bb6667e3fd
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-03 19:46:05 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,14 +1,2 @@
-#1
-#clean-all
-rm -rf /opt/dlami/nvme/sparse_emb_outputs/v2_attn
-rm -rf /opt/dlami/nvme/sparse_emb_outputs/baseline
-rm -rf /opt/dlami/nvme/sparse_emb_outputs/logs
-
-rm -rf ~/.cache/huggingface/datasets
-echo "HF cache removed"
-
-find /opt/dlami/nvme/sparse_emb_data -name "cache-*" -delete 2>/dev/null
-find /opt/dlami/nvme/sparse_emb_data -name "tmp*" -delete 2>/dev/null
-echo "Data cache/tmp files removed"
-
-echo "Done"
+#2 +a
+#pull-latest-log
