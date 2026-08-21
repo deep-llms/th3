a9a2c97e4cf1313408ccc8b6081f3ae9491a70ca
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-08-21 19:06:52 +0000

    commands.sh: install sparse_emb with new runner

--- commands.sh
+++ commands.sh
@@ -1,11 +1,2 @@
-#1
-#th3-setup-env-20260821-185655
-set -euo pipefail
-
-echo '=== th3 environment setup ==='
-date -u
-hostname
-
-bash scripts/setup_env.sh
-
-echo 'TH3 ENVIRONMENT SETUP DONE'
+#i sparse_emb.txt
+#th3-install-project-envs-20260821
